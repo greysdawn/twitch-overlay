8407db3a6f67c44488a4ac208487cc3ac9bde9d2
Grey Himmel <greysdawn@gmail.com>
2024-04-14 19:36:40 +0000

    fix: make sure things work, add assets

--- handlers/subscriptions.js
+++ handlers/subscriptions.js
@@ -1,131 +1,117 @@
-const axios = require('axios');
-const crypto = require('crypto');
-const {
-	ENDPOINTS,
-	SUBS,
-	HEADERS,
-	EVENTS
-} = require('../constants');
-const { sleep } = require('../utils');
+const axios = require("axios");
+const crypto = require("crypto");
+const { ENDPOINTS, SUBS, HEADERS, EVENTS } = require("../constants");
+const { sleep } = require("../utils");
 
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const callbackURL = process.env.CALLBACK_URL;
 const appSecret = process.env.APP_SECRET;
 
+var running;
+
 const Client = axios.create({
 	baseURL: ENDPOINTS.BASE(),
 	headers: {
-		'Client-Id': clientID,
-		'Content-Type': 'application/json'
-	}
-})
+		"Client-Id": clientID,
+		"Content-Type": "application/json",
+	},
+});
 
-function verify (req, res, next) {
-	if(!verifyHash(req.headers, req.body)) return res.status(403).send();
+function verify(req, res, next) {
+	if (!verifyHash(req.headers, req.body)) return res.status(403).send();
 	next();
 }
 
 function verifyHash(headers, body) {
-	var data = headers[HEADERS.ID] +
-			   headers[HEADERS.TIMESTAMP] +
-			   JSON.stringify(body);
+	var data =
+		headers[HEADERS.ID] + headers[HEADERS.TIMESTAMP] + JSON.stringify(body);
 	var sig = headers[HEADERS.SIGNATURE];
 
-	var hash = crypto.createHmac('sha256', appSecret)
-		.update(data)
-		.digest('hex');
-	hash = `sha256=`+hash;
+	var hash = crypto.createHmac("sha256", appSecret).update(data).digest("hex");
+	hash = `sha256=` + hash;
 
 	return crypto.timingSafeEqual(Buffer.from(hash), Buffer.from(sig));
-}
-
-async function getToken() {
-	var query =
-		`client_id=${clientID}` +
-		`&client_secret=${clientSecret}` +
-		`&grant_type=client_credentials`;
-	try {
-		var req = await axios.post(`https://id.twitch.tv/oauth2/token?${query}`);
-		var data = req.data;
-	} catch(e) {
-		console.log(e.config, e.message);
-	}
-	
-	return data.access_token;
 }
 
 async function handleQueue(clients) {
 	running = true;
 
-	if(queue.length) {
-		for(var c of clients) {
+	if (events.length) {
+		for (var c of Object.values(clients)) {
 			c.write(`event: special\n`);
-			c.write(`data: ${JSON.stringify(queue[0])}\n\n`);
+			c.write(`data: ${JSON.stringify(events[0])}\n\n`);
 		}
-		queue.shift();
+		events.shift();
 		await sleep(3000);
 	}
 
-	if(!queue.length) running = false;
+	if (!events.length) running = false;
 	else await handleQueue(clients);
 }
 
 async function clean() {
 	var l = processed.size;
-	for(var [k, v] of processed) {
+	for (var [k, v] of processed) {
 		var cur = new Date();
 		var dt = new Date(v);
-		if(dt < (cur - (2*60*1000))) processed.delete(k);
+		if (dt < cur - 2 * 60 * 1000) processed.delete(k);
 	}
 	console.log(`cleaned ${l - processed.size} processed notifications`);
 }
 
-const events = [ ];
+const events = [];
 const processed = new Map();
 
-module.exports = async function setup(app, evtClients) {
+module.exports = async function setup(app, evtClients, TOKEN) {
 	try {
-		TOKEN = await getToken();
-		Client.defaults.headers['Authorization'] = `Bearer ${TOKEN}`;
+		Client.defaults.headers["Authorization"] = `Bearer ${TOKEN}`;
 
 		var transport = {
-			method: 'webhook',
+			method: "webhook",
 			callback: callbackURL,
-			secret: appSecret
-		}
+			secret: appSecret,
+		};
 
 		var req = await Client.get(ENDPOINTS.GET_SUBSCRIPTIONS());
 		var existing = req.data;
 
-		for(var e of existing.data) {
-			if(e.status == 'enabled') continue;
+		for (var e of existing.data) {
+			if (e.status == "enabled" && e.transport.callback == callbackURL)
+				continue;
 			await Client.delete(ENDPOINTS.DELETE_SUBSCRIPTION(e.id));
 		}
 
-		for(var sub of SUBS) {
-			if(!existing.data.find(s => s.type == sub.type && s.status == "enabled")) {
+		for (var sub of SUBS) {
+			if (
+				!existing.data.find(
+					(s) =>
+						s.type == sub.type &&
+						s.status == "enabled" &&
+						s.transport.callback == callbackURL,
+				)
+			) {
 				await Client.post(ENDPOINTS.CREATE_SUBSCRIPTION(), {
 					...sub,
-					transport
-				})
+					transport,
+				});
 			}
 		}
 
-		app.post('/', verify, (req, res) => {
-			res.set('content-type', 'text/plain');
-			if(processed.has(req.headers[HEADERS.ID])) return res.status(200).send();
+		app.post("/", verify, (req, res) => {
+			res.set("content-type", "text/plain");
+			if (processed.has(req.headers[HEADERS.ID])) return res.status(200).send();
 			processed.set(req.headers[HEADERS.ID], req.headers[HEADERS.TIMESTAMP]);
 
 			var type = req.headers[HEADERS.TYPE];
-			switch(type) {
+			switch (type) {
 				case EVENTS.NOTIFICATION:
 					events.push({
 						id: req.headers[HEADERS.ID],
 						type: req.body.subscription.type,
-						data: req.body.event
-					})
-					if(!running) handleQueue(evtClients);
+						data: req.body.event,
+					});
+					if (!running) handleQueue(evtClients);
 					break;
 				case EVENTS.CHALLENGE:
 					return res.status(200).send(req.body.challenge);
@@ -135,10 +121,10 @@ module.exports = async function setup(app, evtClients) {
 			}
 
 			return res.status(204).send();
-		})
+		});
 
 		setInterval(() => clean(), 2 * 60 * 1000);
-	} catch(e){
-		console.log(e.message, e.response?.data)
+	} catch (e) {
+		console.log(e.message, e.response?.data);
 	}
-}
+};
--- index.js
+++ index.js
@@ -1,41 +1,59 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + "/assets"));
 
-const index = fs.readFileSync('./index.html');
+const index = fs.readFileSync("./index.html");
 app.get("/", async (req, res) => {
-	return res.status(200).send(index.toString('utf-8'));
-})
+	return res.status(200).send(index.toString("utf-8"));
+});
 
 const evtClients = {};
 app.get("/events", async (req, res) => {
-	console.log('New connection request received');
+	console.log("New connection request received");
 	let clientId = req.query.client_id;
 
 	evtClients[clientId] = res;
-	res.socket.on('end', _ => {
+	res.socket.on("end", (_) => {
 		delete evtClients[clientId];
 		res.end();
-	})
+	});
 
 	res.setHeader("Content-Type", "text/event-stream");
 	res.write(`event: connect\n`);
 	res.write(`data: Connection established.\n\n`);
-})
+});
+
+async function getToken() {
+	var query =
+		`client_id=${process.env.CLIENT_ID}` +
+		`&client_secret=${process.env.CLIENT_SECRET}` +
+		`&grant_type=client_credentials`;
+	try {
+		var req = await axios.post(`https://id.twitch.tv/oauth2/token?${query}`);
+		var data = req.data;
+	} catch (e) {
+		console.log(e.config, e.message);
+	}
+
+	return data.access_token;
+}
 
 async function setup() {
-	var files = await fs.readdirSync('./handlers');
-	for(var f of files) {
-		var handler = require(f);
-		if(f.setup) await f.setup(app, evtClients);
+	const TOKEN = await getToken();
+	var files = fs.readdirSync(__dirname + "/handlers");
+	for (var f of files) {
+		var fn = `${__dirname}/handlers/${f}`;
+		var handler = require(fn);
+		await handler(app, evtClients, TOKEN);
 	}
 }
 
 setup();
 
 const PORT = process.env.PORT ?? 8080;
-app.listen(PORT)
+app.listen(PORT);
 console.log(`App listening on port ${PORT}`);
